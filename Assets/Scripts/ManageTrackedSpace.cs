cd76159fe407a4dc4177a9cb744a4198204033f1
Henry ORange <orangehill7@gmail.com>
2024-12-05 23:07:01 +0100

    started to implement getting guardian borders

---
+++ Assets/Scripts/ManageTrackedSpace.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Meta.XR.MRUtilityKit;
+using Unity.XR.Oculus;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class ManageTrackedSpace : MonoBehaviour
+{
+    private OVRBoundary boundary;
+    private Vector3[] points;
+
+    void Start()
+    {
+        boundary = OVRManager.boundary;
+        points = boundary.GetGeometry(OVRBoundary.BoundaryType.PlayArea);
+        foreach (var point in points)
+        {
+            Debug.LogError(point);
+        }
+        
+        foreach (var point in points)
+        {
+            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = point;
+            cube.transform.localScale = new Vector3(0.1f, 1f, 0.1f);
+            cube.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    { MRUK
+        // maybe with room setup?
+        // var room = MRUK.Instance?.GetCurrentRoom();
+        // foreach (var point in room.GetRoomOutline())
+        // {
+        //     Debug.LogError(point.ToString());
+        // }
+        // foreach (var point in room.GetRoomOutline())
+        // {
+        //     var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        //     cube.transform.position = point;
+        //     cube.transform.localScale = new Vector3(0.1f, 1f, 0.1f);
+        //     cube.GetComponent<MeshRenderer>().material.color = Color.red;
+        // }
+    }
+}
